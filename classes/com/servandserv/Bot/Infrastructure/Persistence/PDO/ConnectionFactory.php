795e963638699b65d0559087f00a88124fffe5d3
servandserv <servandserv@gmail.com>
2016-12-16 16:32:32 +0400

    set input param as url

--- classes/com/servandserv/Bot/Infrastructure/Persistence/PDO/ConnectionFactory.php
+++ classes/com/servandserv/Bot/Infrastructure/Persistence/PDO/ConnectionFactory.php
@@ -8,25 +8,38 @@ class ConnectionFactory
     private static $instance;
     private $pdo;
 
-    private function __construct( $dns, $user, $pass, $opt )
+    private function __construct( $url, $user, $pass, $opt = NULL )
     {
-        $opt = [
-            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
-        ];
+        if( !$opt ) {
+            $opt = [
+                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            ];
+        }
         try {
-            $this->pdo = new \PDO( $dns, $user, $pass, $opt );
+            $this->pdo = new \PDO( $this->dns( $url ), $user, $pass, $opt );
         } catch ( \PDOException $e ) {
             die( "Connection error: ".$e->getMessage() );
         }
     }
 
-    public static function getConnect( $dns, $user, $pass, $opt )
+    public static function getConnect( $url, $user, $pass, $opt = NULL )
     {
         if( NULL == self::$instance ) {
-            self::$instance = new self( $dns, $user, $pass, $opt );
+            self::$instance = new self( $url, $user, $pass, $opt );
         }
         return self::$instance->pdo;
+    }
+    
+    // "mysql:host=localhost;dbname=bbbot;charset=utf8"
+    private function dns( $url )
+    {
+        $url = parse_url( $url );
+        return $url["scheme"].
+            ":host=".$url["host"].
+            (isset($url["port"])?":".$url["port"]:"").
+            ";dbname=".substr($url["path"],1).
+            ";charset=utf8";
     }
     
 }
