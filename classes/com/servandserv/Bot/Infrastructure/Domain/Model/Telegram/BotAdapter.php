3d1c44c7562d6c3732c2902f7ec16c721a1fabcc
servandserv <servandserv@gmail.com>
2016-12-18 15:26:09 +0400

    адаптеры для запросов на webhook со стороны Telegram

--- classes/com/servandserv/Bot/Infrastructure/Domain/Model/Telegram/BotAdapter.php
+++ classes/com/servandserv/Bot/Infrastructure/Domain/Model/Telegram/BotAdapter.php
@@ -3,6 +3,15 @@
 namespace com\servandserv\Bot\Infrastructure\Domain\Model\Telegram;
 
 use \com\servandserv\Bot\Domain\Model\CurlClient;
+use \org\telegram\data\bot\Update as TelegramUpdate;
+use \com\servandserv\data\bot\Updates;
+use \com\servandserv\data\bot\Update;
+use \com\servandserv\data\bot\Chat;
+use \com\servandserv\data\bot\User;
+use \com\servandserv\data\bot\Message;
+use \com\servandserv\data\bot\Location;
+use \com\servandserv\data\bot\Contact;
+use \com\servandserv\data\bot\Command;
 
 class BotAdapter implements \com\servandserv\Bot\Domain\Model\BotPort
 {
@@ -44,32 +53,132 @@ class BotAdapter implements \com\servandserv\Bot\Domain\Model\BotPort
     public function getUpdates()
     {
         if( NULL == self::$updates ) {
+            self::$updates = ( new Updates() )->setContext( self::CONTEXT );
             $in = file_get_contents( "php://input" );
+            if( !$json = json_decode( $in, TRUE ) ) throw new \Exception( "Error on decode update json in ".__FILE__." on line ".__LINE__ );
+            $up = $this->translateToUpdate( ( new TelegramUpdate() )->fromJSONArray( $json ) );
+            self::$updates->setUpdate( $up );
+        }
+        
+        return self::$updates;
+    }
+    
+    private function translateToUpdate( \org\telegram\data\bot\Update $tup )
+    {
+        $up = ( new Update() )->setId( $tup->getUpdate_id() );
+        $up->setChat( ( new Chat() )->setContext( self::CONTEXT ) );
+        $this->fromCallbackQueryType( $tup->getCallback_query(), $up );
+        $this->fromMessageType( $tup->getMessage(), $up );
+        
+        return $up;
+    }
+    
+    
+    private function fromCallbackQueryType( \org\telegram\data\bot\CallbackQueryType $cbq = NULL, Update $up )
+    {
+        if( !$cbq ) return NULL;
+        $com = ( new Command() )->setName( $cbq->getData() );
+        $this->fromUserType( $cbq->getFrom(), $up->getChat() );
+        $up->setCommand( $com );
+    }
+    
+    private function fromUserType( \org\telegram\data\bot\UserType $from = NULL, Chat $chat )
+    {
+        if( !$from ) return NULL;
+        $u = ( new User() )
+            ->setId( $from->getId() )
+            ->setFirstName( $from->getFirst_name() )
+            ->setLastName( $from->getLast_name() )
+            ->setNickname( $from->getUsername() );
+        
+        $chat->setUser( $u );
+    }
+    
+    
+    private function fromMessageType( \org\telegram\data\bot\MessageType $mt = NULL, Update $up )
+    {
+        if( !$mt ) return NULL;
+        $m = ( new Message() )
+            ->setId( $mt->getMessage_id() )
+            ->setDt( $mt->getDate() )
+            ->setText( $mt->getText() );
+        $this->fromLocationType( $mt->getLocation(), $m, $up->getChat() );
+        $this->fromContactType( $mt->getContact(), $m, $up->getChat() );
+        $this->fromUserType( $mt->getFrom(), $up->getChat() );
+        $this->fromEntities( $mt->getEntities(), $m, $up );
+        $this->fromChatType( $mt->getChat(), $up->getChat() );
+        $m->setUser( $up->getChat()->getUser() );
+        $up->setMessage( $m );
+    }
+        
+    private function fromEntities( array $entities, Message $m, Update $up )
+    {
+        foreach( $entities as $entity )
+        {
+            if( $entity->getType() === "bot_command" && $m->getText() ) {
+                $name = substr( $m->getText(), intval( $entity->getOffset() ) + 1, intval( $entity->getOffset() ) + intval( $entity->getLength() ) );
+                $arg = substr( $m->getText(), intval( $entity->getOffset() ) + 1 + intval( $entity->getLength() ) );
+                if( !$com = $up->getCommand() ) $com = new Command();
+                $com->setName( $name )->setArguments( $arg );
+                $up->setCommand( $com );
+                
+                return ;
+            }
+        }
+    }
+
+    private function fromLocationType( \org\telegram\data\bot\LocationType $lt = NULL, Message $m, Chat $chat )
+    {
+        if( !$lt ) return NULL;
+        $l = ( new Location() )
+            ->setLatitude( $lt->getLatitude() )
+            ->setLongitude( $lt->getLongitude() );
+        
+        $chat->setLocation( $l );
+        $m->setLocation( $l );
+    }
+    
+    private function fromContactType( \org\telegram\data\bot\ContactType $ct = NULL, Message $m, Chat $chat )
+    {
+        if( !$ct ) return NULL;
+        $c = ( new Contact() )
+            ->setPhoneNumber( str_replace( ["+","(",")","[","]","-"], "", $ct->getPhone_number() ) );
+        $chat->setContact( $c );
+        $m->setContact( $c );
+    }
+    
+    private function fromChatType( \org\telegram\data\bot\ChatType $cht, Chat $chat )
+    {
+        if( !$cht ) return NULL;
+        $chat->setId( $cht->getId() )
+            ->setType( $cht->getType() )
+            ->setUser( ( new User() )
+                ->setFirstName( $cht->getFirst_name() )
+                ->setLastName( $cht->getLast_name() )
+                ->setNickname( $cht->getUsername() )
+                ->setId( $cht->getId() ) 
+            );
+    }
+    
+    /**
+    public function getUpdates()
+    {
+        if( NULL == self::$updates ) {
+            $in = file_get_contents( "php://input" );
+            
             self::$updates = ( new \com\servandserv\data\bot\Updates() )->setContext( self::CONTEXT );
             if( !$json = json_decode( $in, TRUE ) ) throw new \Exception( "Error on decode update json in ".__FILE__." on line ".__LINE__ );
             $up = ( new \com\servandserv\data\bot\Update() )->setContext( self::CONTEXT );
             $chat = new \com\servandserv\data\bot\Chat();
-            $up->setId( $json["update_id"] );
-            if( isset( $json["callback_query"] ) ) {
-                $up->setEvent("CallbackReceived");
-                if( isset( $json["callback_query"]["data"] ) ) {
-                    $com = new \com\servandserv\data\bot\Command();
-                    $com->setName( $json["callback_query"]["data"] );
-                    $up->setCommand( $com );
-                }
-                if( isset( $json["callback_query"]["from"] ) ) {
-                    $chat->setUser( $this->userFromJSON( $json["callback_query"]["from"] ) );
-                }
-                if( isset( $json["callback_query"]["chat_instance"] ) ) {
-                    $chat->setId( $json["callback_query"]["chat_instance"] );
-                }
-            }
-            if(isset( $json["message"] ) ) {
+            if( isset( $json["update_id"] ) ) $up->setId( $json["update_id"] );
+            if( isset( $json["message"] ) ) {
                 $up->setEvent( "MessageReceived" );
                 $m = new \com\servandserv\data\bot\Message();
-                $m->setId( $json["message"]["message_id"] );
-                $m->setDt( $json["message"]["date"] );
-                $chat = $this->chatFromJSON( $json["message"]["chat"] );
+                if( isset( $json["message"]["message_id"] ) ) $m->setId( $json["message"]["message_id"] );
+                if( isset( $json["message"]["date"] ) ) $m->setDt( $json["message"]["date"] );
+                if( isset( $json["message"]["chat"] ) ) {
+                    $chat = $this->chatFromJSON( $json["message"]["chat"] );
+                }
                 if( isset( $json["message"]["text"] ) ) {
                     $m->setText( $json["message"]["text"] );
                 }
@@ -105,6 +214,7 @@ class BotAdapter implements \com\servandserv\Bot\Domain\Model\BotPort
         }
         return self::$updates;
     }
+    */
     
     public function response( \com\servandserv\happymeal\XML\Schema\AnyType $anyType = NULL, $code = 200 )
     {
